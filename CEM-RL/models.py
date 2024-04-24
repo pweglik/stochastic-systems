8bfc7151e5cee5d7d7fa469860a56b1f912bb4a1
pweglik <przemek.weglik@onet.pl>
2024-04-24 14:53:31 +0200

    Cleaning up the code a little bit

--- CEM-RL/models.py
+++ CEM-RL/models.py
@@ -82,72 +82,137 @@ class RLNN(nn.Module):
 
 
 class Actor(RLNN):
-
-    def __init__(self, state_dim, action_dim, max_action, layer_norm=False, init=True):
+    def __init__(self, state_dim, action_dim, max_action, args):
         super(Actor, self).__init__(state_dim, action_dim, max_action)
+        self.args = args
 
         self.l1 = nn.Linear(state_dim, 400)
         self.l2 = nn.Linear(400, 300)
         self.l3 = nn.Linear(300, action_dim)
 
-        if layer_norm:
+        if self.args.layer_norm:
             self.n1 = nn.LayerNorm(400)
             self.n2 = nn.LayerNorm(300)
-        self.layer_norm = layer_norm
+        self.layer_norm = args.layer_norm
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.args.actor_lr)
+        self.tau = self.args.tau
+        self.discount = self.args.discount
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.max_action = max_action
 
     def forward(self, x):
 
         if not self.layer_norm:
             x = torch.tanh(self.l1(x))
             x = torch.tanh(self.l2(x))
-            x = self.max_action * torch.tanh(self.l3(x))
+            x = self.max_action * F.tanh(self.l3(x))
 
         else:
             x = torch.tanh(self.n1(self.l1(x)))
             x = torch.tanh(self.n2(self.l2(x)))
-            x = self.max_action * torch.tanh(self.l3(x))
+            x = self.max_action * F.tanh(self.l3(x))
 
         return x
 
+    def update(self, memory, batch_size, critic, actor_t):
+
+        # Sample replay buffer
+        states, _, _, _, _ = memory.sample(batch_size)
+
+        # Compute actor loss
+        if self.args.use_td3:
+            actor_loss = -critic(states, self(states))[0].mean()
+        else:
+            actor_loss = -critic(states, self(states)).mean()
+
+        # Optimize the actor
+        self.optimizer.zero_grad()
+        actor_loss.backward()
+        self.optimizer.step()
+
+        # Update the frozen target models
+        for param, target_param in zip(self.parameters(), actor_t.parameters()):
+            target_param.data.copy_(
+                self.tau * param.data + (1 - self.tau) * target_param.data
+            )
+
 
 class Critic(RLNN):
-    def __init__(self, state_dim, action_dim, layer_norm=False):
+    def __init__(self, state_dim, action_dim, max_action, args):
         super(Critic, self).__init__(state_dim, action_dim, 1)
+        self.args = args
 
         self.l1 = nn.Linear(state_dim + action_dim, 400)
         self.l2 = nn.Linear(400, 300)
         self.l3 = nn.Linear(300, 1)
 
-        if layer_norm:
+        if self.args.layer_norm:
             self.n1 = nn.LayerNorm(400)
             self.n2 = nn.LayerNorm(300)
-        self.layer_norm = layer_norm
+
+        self.layer_norm = self.args.layer_norm
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.args.critic_lr)
+        self.tau = self.args.tau
+        self.discount = self.args.discount
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.max_action = max_action
 
     def forward(self, x, u):
 
         if not self.layer_norm:
-            x = torch.leaky_relu(self.l1(torch.cat([x, u], 1)))
-            x = torch.leaky_relu(self.l2(x))
+            x = F.leaky_relu(self.l1(torch.cat([x, u], 1)))
+            x = F.leaky_relu(self.l2(x))
             x = self.l3(x)
 
         else:
-            x = torch.leaky_relu(self.n1(self.l1(torch.cat([x, u], 1))))
-            x = torch.leaky_relu(self.n2(self.l2(x)))
+            x = F.leaky_relu(self.n1(self.l1(torch.cat([x, u], 1))))
+            x = F.leaky_relu(self.n2(self.l2(x)))
             x = self.l3(x)
 
         return x
 
+    def update(self, memory, batch_size, actor_t, critic_t):
+
+        # Sample replay buffer
+        states, n_states, actions, rewards, dones = memory.sample(batch_size)
+
+        # Q target = reward + discount * Q(next_state, pi(next_state))
+        with torch.no_grad():
+            target_Q = critic_t(n_states, actor_t(n_states))
+            target_Q = rewards + (1 - dones) * self.discount * target_Q
+
+        # Get current Q estimate
+        current_Q = self(states, actions)
+
+        # Compute critic loss
+        critic_loss = nn.MSELoss()(current_Q, target_Q)
+
+        # Optimize the critic
+        self.optimizer.zero_grad()
+        critic_loss.backward()
+        self.optimizer.step()
+
+        # Update the frozen target models
+        for param, target_param in zip(self.parameters(), critic_t.parameters()):
+            target_param.data.copy_(
+                self.tau * param.data + (1 - self.tau) * target_param.data
+            )
+
 
 class CriticTD3(RLNN):
-    def __init__(self, state_dim, action_dim, layer_norm=False):
+    def __init__(self, state_dim, action_dim, max_action, args):
         super(CriticTD3, self).__init__(state_dim, action_dim, 1)
+        self.args = args
 
         # Q1 architecture
         self.l1 = nn.Linear(state_dim + action_dim, 400)
         self.l2 = nn.Linear(400, 300)
         self.l3 = nn.Linear(300, 1)
 
-        if layer_norm:
+        if self.args.layer_norm:
             self.n1 = nn.LayerNorm(400)
             self.n2 = nn.LayerNorm(300)
 
@@ -156,31 +221,79 @@ class CriticTD3(RLNN):
         self.l5 = nn.Linear(400, 300)
         self.l6 = nn.Linear(300, 1)
 
-        if layer_norm:
+        if self.args.layer_norm:
             self.n4 = nn.LayerNorm(400)
             self.n5 = nn.LayerNorm(300)
-        self.layer_norm = layer_norm
+
+        self.layer_norm = self.args.layer_norm
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.args.critic_lr)
+        self.tau = self.args.tau
+        self.discount = self.args.discount
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.max_action = max_action
+        self.policy_noise = self.args.policy_noise
+        self.noise_clip = self.args.noise_clip
 
     def forward(self, x, u):
 
         if not self.layer_norm:
-            x1 = torch.leaky_relu(self.l1(torch.cat([x, u], 1)))
-            x1 = torch.leaky_relu(self.l2(x1))
+            x1 = F.leaky_relu(self.l1(torch.cat([x, u], 1)))
+            x1 = F.leaky_relu(self.l2(x1))
             x1 = self.l3(x1)
 
         else:
-            x1 = torch.leaky_relu(self.n1(self.l1(torch.cat([x, u], 1))))
-            x1 = torch.leaky_relu(self.n2(self.l2(x1)))
+            x1 = F.leaky_relu(self.n1(self.l1(torch.cat([x, u], 1))))
+            x1 = F.leaky_relu(self.n2(self.l2(x1)))
             x1 = self.l3(x1)
 
         if not self.layer_norm:
-            x2 = torch.leaky_relu(self.l4(torch.cat([x, u], 1)))
-            x2 = torch.leaky_relu(self.l5(x2))
+            x2 = F.leaky_relu(self.l4(torch.cat([x, u], 1)))
+            x2 = F.leaky_relu(self.l5(x2))
             x2 = self.l6(x2)
 
         else:
-            x2 = torch.leaky_relu(self.n4(self.l4(torch.cat([x, u], 1))))
-            x2 = torch.leaky_relu(self.n5(self.l5(x2)))
+            x2 = F.leaky_relu(self.n4(self.l4(torch.cat([x, u], 1))))
+            x2 = F.leaky_relu(self.n5(self.l5(x2)))
             x2 = self.l6(x2)
 
         return x1, x2
+
+    def update(self, memory, batch_size, actor_t, critic_t):
+
+        # Sample replay buffer
+        states, n_states, actions, rewards, dones = memory.sample(batch_size)
+
+        # Select action according to policy and add clipped noise
+        noise = np.clip(
+            np.random.normal(0, self.policy_noise, size=(batch_size, self.action_dim)),
+            -self.noise_clip,
+            self.noise_clip,
+        )
+        n_actions = actor_t(n_states) + FloatTensor(noise)
+        n_actions = n_actions.clamp(-self.max_action, self.max_action)
+
+        # Q target = reward + discount * min_i(Qi(next_state, pi(next_state)))
+        with torch.no_grad():
+            target_Q1, target_Q2 = critic_t(n_states, n_actions)
+            target_Q = torch.min(target_Q1, target_Q2)
+            target_Q = rewards + (1 - dones) * self.discount * target_Q
+
+        # Get current Q estimates
+        current_Q1, current_Q2 = self(states, actions)
+
+        # Compute critic loss
+        critic_loss = nn.MSELoss()(current_Q1, target_Q) + nn.MSELoss()(
+            current_Q2, target_Q
+        )
+
+        # Optimize the critic
+        self.optimizer.zero_grad()
+        critic_loss.backward()
+        self.optimizer.step()
+
+        # Update the frozen target models
+        for param, target_param in zip(self.parameters(), critic_t.parameters()):
+            target_param.data.copy_(
+                self.tau * param.data + (1 - self.tau) * target_param.data
+            )
